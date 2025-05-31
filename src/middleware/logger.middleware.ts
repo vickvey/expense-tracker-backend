a8ae1f3724a668188e7b6b181a45c2eb5b9e0304
vickvey <vickvey0303@gmail.com>
2025-05-31 16:04:27 +0530

    added logger middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,11 +1,8 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from 'express';
 
-const loggerMiddleware = (
-    req: Request,
-    res: Response,
-    next: NextFunction) => {
-    console.log(`[REQ] - ${req.method} - ${req.originalUrl}`);
-    next();
-}
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[REQ] - ${req.method} - ${req.originalUrl}`);
+  next();
+};
 
 export default loggerMiddleware;
---
+++ src/models/user.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        maxlength: 255
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Enter a valid email address"],
+        maxLength: 255
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: 4,
+        maxlength: 255
+    }
+})
