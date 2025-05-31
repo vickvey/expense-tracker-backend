f6b5ab3423cc1bb78a59e4b26de3c050846bffea
vickvey <vickvey0303@gmail.com>
2025-05-31 16:46:53 +0530

    fixed custom error prototype chain

--- src/lib/customError.ts
+++ src/lib/customError.ts
@@ -2,6 +2,8 @@ export class CustomError extends Error {
   statusCode: number;
   constructor(message: string, statusCode: number) {
     super(message);
+    this.name = 'CustomError';
     this.statusCode = statusCode;
+    Object.setPrototypeOf(this, CustomError.prototype); // Ensure prototype chain
   }
 }
