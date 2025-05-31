ccd9aaf90eec3bb55db1a7e2bebb377f5de4a419
vickvey <vickvey0303@gmail.com>
2025-05-31 16:05:14 +0530

    added api response class

--- src/lib/apiResponse.ts
+++ src/lib/apiResponse.ts
@@ -2,38 +2,46 @@
 import { Response } from 'express';
 
 export class ApiResponse {
-    static send<T>(
-        res: Response,
-        statusCode: number,
-        success: boolean,
-        message: string,
-        data?: T
-    ) {
-        console.log(`[RES] - ${success ? 'success' : 'error'} - ${res.statusCode} - ${message}`);
-        return res.status(statusCode).json({
-            success,
-            message,
-            data,
-        });
+  static send<T>(
+    res: Response,
+    statusCode: number,
+    success: boolean,
+    message: string,
+    data?: T,
+  ) {
+    if (success) {
+      console.log(
+        `[RES] - ${success ? 'success' : 'error'} - ${res.statusCode} - ${message}`,
+      );
+    } else {
+      console.error(
+        `[RES] - ${success ? 'success' : 'error'} - ${res.statusCode} - ${message}`,
+      );
     }
+    res.status(statusCode).json({
+      success,
+      message,
+      data,
+    });
+  }
 
-    // Optional shortcut method for successful response
-    static success<T>(
-        res: Response,
-        statusCode = 200,
-        message: string,
-        data?: T
-    ) {
-        return this.send(res, 200, true, message, data);
-    }
+  // Optional shortcut method for successful response
+  static success<T>(
+    res: Response,
+    statusCode = 200,
+    message: string,
+    data?: T,
+  ) {
+    this.send(res, 200, true, message, data);
+  }
 
-    // Optional shortcut for error
-    static error(
-        res: Response,
-        message: string,
-        statusCode = 400,
-        errorData?: any
-    ) {
-        return this.send(res, statusCode, false, message, errorData);
-    }
+  // Optional shortcut for error
+  static error(
+    res: Response,
+    message: string,
+    statusCode = 400,
+    errorData?: any,
+  ) {
+    this.send(res, statusCode, false, message, errorData);
+  }
 }
