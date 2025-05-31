5d58ec129f2e8608f063e27e0118a5cd4ffcd734
vickvey <vickvey0303@gmail.com>
2025-05-31 16:07:24 +0530

    modified server and index and added boilerplate routes

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,30 @@
-import express, {Request, Response} from "express";
-import {ApiResponse} from "./lib/apiResponse";
-import loggerMiddleware from "./middleware/logger.middleware";
+import express, { Request, Response } from 'express';
+import { ApiResponse } from './lib/apiResponse';
+import loggerMiddleware from './middleware/logger.middleware';
+import errorMiddleware from './middleware/error.middleware';
+import authRouter from './routes/auth.routes';
+import userRouter from './routes/user.routes';
+import categoryRouter from './routes/category.routes';
+import transactionRouter from './routes/transaction.routes';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
 
-
 app.get('/', (req, res) => {
-    ApiResponse.success(res, 200, 'Expense Tracker API is live !!');
+  ApiResponse.success(res, 200, 'Expense Tracker API is live !!');
 });
+
+// app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/user', userRouter);
+// app.use('/api/v1/category', categoryRouter);
+// app.use('/api/v1/transaction', transactionRouter);
 
 app.all('/*splat', (req: Request, res: Response) => {
-    ApiResponse.error(res, 'Not Found', 404);
+  ApiResponse.error(res, 'Not Found', 404);
 });
 
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 export default app;
